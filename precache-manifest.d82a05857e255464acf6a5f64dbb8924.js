8da15b64deb83d180179303b91e4e8ceb27b441e
Jesús Wisord <wisord.autumn@gmail.com>
2019-09-30 10:07:37 -0500

    Updates

---
+++ precache-manifest.d82a05857e255464acf6a5f64dbb8924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4db39dc66f960f5d323b021fe7110f8",
+    "url": "/Wisordme/Pokedex/index.html"
+  },
+  {
+    "revision": "ed67b07cfa823b28a774",
+    "url": "/Wisordme/Pokedex/static/css/main.1f07d5ec.chunk.css"
+  },
+  {
+    "revision": "fd3f3237f5f97e8c6006",
+    "url": "/Wisordme/Pokedex/static/js/2.785f69f2.chunk.js"
+  },
+  {
+    "revision": "ed67b07cfa823b28a774",
+    "url": "/Wisordme/Pokedex/static/js/main.60596ee4.chunk.js"
+  },
+  {
+    "revision": "7e78f3521d3188af5e95",
+    "url": "/Wisordme/Pokedex/static/js/runtime~main.f606b7c3.js"
+  }
+]);
